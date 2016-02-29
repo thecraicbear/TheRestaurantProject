98a56012a574205ed07b3b1d0c11a406f5cf8ceb
Dale Omara <ITDOMARA586@students.sthelens.ac.uk>
2016-02-29 12:00:31 +0000

    Start Up

--- Website/commonMods/_vti_cnf/content_Index.php
+++ Website/commonMods/_vti_cnf/content_Index.php
@@ -1,13 +1,13 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|STUDENTS\\itjblund592
-vti_modifiedby:SR|STUDENTS\\itjblund592
-vti_timelastmodified:TR|26 Feb 2016 10:58:40 -0000
-vti_timecreated:TR|26 Feb 2016 09:18:37 -0000
+vti_modifiedby:SR|STUDENTS\\itdomara586
+vti_timelastmodified:TR|29 Feb 2016 11:59:26 -0000
+vti_timecreated:TR|26 Feb 2016 09:19:25 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|
-vti_nexttolasttimemodified:TW|26 Feb 2016 10:58:31 -0000
-vti_cacheddtm:TX|26 Feb 2016 10:58:42 -0000
-vti_filesize:IR|469
+vti_nexttolasttimemodified:TR|29 Feb 2016 11:57:13 -0000
+vti_cacheddtm:TX|29 Feb 2016 11:57:13 -0000
+vti_filesize:IR|287
 vti_cachedbodystyle:SR|<body>
 vti_cachedneedsrewrite:BR|false
 vti_cachedhasbots:BR|false
--- Website/commonMods/_vti_cnf/menu.php
+++ Website/commonMods/_vti_cnf/menu.php
@@ -1,12 +1,12 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|STUDENTS\\itjblund592
-vti_modifiedby:SR|STUDENTS\\itjblund592
-vti_timelastmodified:TR|26 Feb 2016 11:01:43 -0000
+vti_modifiedby:SR|STUDENTS\\itdomara586
+vti_timelastmodified:TR|29 Feb 2016 11:59:26 -0000
 vti_timecreated:TR|26 Feb 2016 09:20:29 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|
-vti_nexttolasttimemodified:TW|26 Feb 2016 11:01:00 -0000
-vti_cacheddtm:TX|26 Feb 2016 11:01:02 -0000
+vti_nexttolasttimemodified:TR|29 Feb 2016 11:57:13 -0000
+vti_cacheddtm:TX|29 Feb 2016 11:57:13 -0000
 vti_filesize:IR|337
 vti_cachedbodystyle:SR|<body>
 vti_cachedneedsrewrite:BR|false
