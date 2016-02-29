520701f3c02e2448709bca741b9d526c45bc164f
J Blundell <itjblund592@students.sthelens.ac.uk>
2016-02-29 12:32:29 +0000

    Corrected Draft
    Changed from first draft

--- commonMods/_vti_cnf/footer.php
+++ commonMods/_vti_cnf/footer.php
@@ -1,12 +1,12 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|STUDENTS\\itjblund592
-vti_modifiedby:SR|STUDENTS\\itdomara586
-vti_timelastmodified:TR|29 Feb 2016 11:59:26 -0000
+vti_modifiedby:SR|STUDENTS\\itjblund592
+vti_timelastmodified:TR|26 Feb 2016 10:44:52 -0000
 vti_timecreated:TR|26 Feb 2016 09:18:51 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|
-vti_nexttolasttimemodified:TR|29 Feb 2016 11:57:13 -0000
-vti_cacheddtm:TX|29 Feb 2016 11:57:13 -0000
+vti_nexttolasttimemodified:TW|26 Feb 2016 10:20:59 -0000
+vti_cacheddtm:TX|26 Feb 2016 10:44:54 -0000
 vti_filesize:IR|384
 vti_cachedbodystyle:SR|<body>
 vti_cachedneedsrewrite:BR|false
