4e15986a87f9f82519e0ea752a925234f3bca1b9
Dale Omara <ITDOMARA586@students.sthelens.ac.uk>
2016-03-08 11:54:58 +0000

    google map

--- commonMods/_vti_cnf/footer.php
+++ commonMods/_vti_cnf/footer.php
@@ -1,13 +1,13 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|STUDENTS\\itjblund592
-vti_modifiedby:SR|STUDENTS\\itjblund592
-vti_timelastmodified:TR|26 Feb 2016 10:44:52 -0000
+vti_modifiedby:SR|STUDENTS\\itdomara586
+vti_timelastmodified:TR|08 Mar 2016 11:29:54 -0000
 vti_timecreated:TR|26 Feb 2016 09:18:51 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|
-vti_nexttolasttimemodified:TW|26 Feb 2016 10:20:59 -0000
-vti_cacheddtm:TX|26 Feb 2016 10:44:54 -0000
-vti_filesize:IR|384
+vti_nexttolasttimemodified:TW|08 Mar 2016 11:28:56 -0000
+vti_cacheddtm:TX|08 Mar 2016 11:29:54 -0000
+vti_filesize:IR|385
 vti_cachedbodystyle:SR|<body>
 vti_cachedneedsrewrite:BR|false
 vti_cachedhasbots:BR|false
--- commonMods/footer.php
+++ commonMods/footer.php
@@ -1,16 +1,25 @@
 <head>
 
-<style type="text/css">.footerContainer {
+<style type="text/css">
+.footerContainer {
 	font-family: Arial, Helvetica, sans-serif;
 	font-size: medium;
 	border: thick double #f1dca7;
-	border-radius: 3px;
-	padding: 5px;
-	padding-bottom:100px;
-	margin:13px;
+	margin: 13px;
 	width: inherit;
-	margin-left:100px;
-	margin-right:100px;
+	margin-left: 100px;
+	margin-right: 100px;
+	padding-top: 5px;
+	padding-right: 5px;
+	padding-bottom: 210px;
+	padding-left: 5px;
+}
+.google_map {
+	border-width: 0px;
+	width: 400px;
+	height: 205px;
+	float: right;
+}
 
 
 </style>
@@ -21,8 +30,8 @@
 
 <div class="footerContainer">
 
-
-</div>
+<iframe class="google_map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4966.173506301821!2d-0.12558715263119208!3d51.51162438986777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487604cb878e81b3%3A0x91f10fa364452046!2sCovent+Garden%2C+London+WC2E!5e0!3m2!1sen!2suk!4v1457437644176"  frameborder="0" style="border:0" ></iframe>
+	</div>
 
 
 
