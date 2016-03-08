84410e0acc46db6c66442f9562f53964df8eff31
Dale Omara <ITDOMARA586@students.sthelens.ac.uk>
2016-03-08 12:11:11 +0000

    opening times added

--- commonMods/footer.php
+++ commonMods/footer.php
@@ -1,5 +1,7 @@
 <head>
 
+<meta content="en-gb" http-equiv="Content-Language">
+
 <style type="text/css">
 .footerContainer {
 	font-family: Arial, Helvetica, sans-serif;
@@ -22,6 +24,13 @@
 }
 
 
+.opening_times {
+	font-family: Broadway;
+	font-size: x-large;
+	float: left;
+	color: #f1dca7;
+}
+
 </style>
 
 </head>
@@ -30,7 +39,15 @@
 
 <div class="footerContainer">
 
-<iframe class="google_map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4966.173506301821!2d-0.12558715263119208!3d51.51162438986777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487604cb878e81b3%3A0x91f10fa364452046!2sCovent+Garden%2C+London+WC2E!5e0!3m2!1sen!2suk!4v1457437644176"  frameborder="0" style="border:0" ></iframe>
+<iframe class="google_map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4966.173506301821!2d-0.12558715263119208!3d51.51162438986777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487604cb878e81b3%3A0x91f10fa364452046!2sCovent+Garden%2C+London+WC2E!5e0!3m2!1sen!2suk!4v1457437644176"
+  frameborder="0" style="border:0" ></iframe>
+  	
+<p  	class="opening_times">
+	<strong>Opening Times</strong><br>
+	Monday-Friday Lunch 12:00-14:30<br>
+	Monday-Saturday Dinner 17:00-22:00<br>
+	Sunday 13:00-21:00<br>
+	</p>
 	</div>
 
 
