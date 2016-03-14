8d100c6a213dd9953bfbe59a00f8c7af5cfdc403
J Blundell <itjblund592@students.sthelens.ac.uk>
2016-03-14 11:18:41 +0000

    Further amendments to events and layout

--- commonMods/_vti_cnf/content_Index.php
+++ commonMods/_vti_cnf/content_Index.php
@@ -1,12 +1,12 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|STUDENTS\\itjblund592
 vti_modifiedby:SR|STUDENTS\\itjblund592
-vti_timelastmodified:TR|11 Mar 2016 10:17:19 -0000
+vti_timelastmodified:TR|11 Mar 2016 10:24:58 -0000
 vti_timecreated:TR|26 Feb 2016 09:19:25 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|
 vti_nexttolasttimemodified:TW|11 Mar 2016 10:13:20 -0000
-vti_cacheddtm:TX|11 Mar 2016 10:17:20 -0000
+vti_cacheddtm:TX|11 Mar 2016 10:24:58 -0000
 vti_filesize:IR|287
 vti_cachedbodystyle:SR|<body>
 vti_cachedneedsrewrite:BR|false
