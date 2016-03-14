dd91ab262fa6526aebd4ef7ee8d48331a5c381c9
Dale Omara <ITDOMARA586@students.sthelens.ac.uk>
2016-03-14 12:46:28 +0000

    find us page

---
+++ commonMods/_vti_cnf/content_Find_us.php
@@ -0,0 +1,18 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|14 Mar 2016 12:46:12 -0000
+vti_author:SR|STUDENTS\\itdomara586
+vti_modifiedby:SR|STUDENTS\\itdomara586
+vti_nexttolasttimemodified:TW|14 Mar 2016 12:44:04 -0000
+vti_timecreated:TR|14 Mar 2016 12:46:12 -0000
+vti_cacheddtm:TX|14 Mar 2016 12:46:12 -0000
+vti_filesize:IR|1589
+vti_cachedbodystyle:SR|<body>
+vti_cachedlinkinfo:VX|S|https://www.google.com/maps/embed S|images/find_us.jpg
+vti_cachedsvcrellinks:VX|NSSS|https://www.google.com/maps/embed NSUS|commonMods/images/find_us.jpg
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
---
+++ commonMods/content_Find_us.php
@@ -0,0 +1,56 @@
+<head>
+
+<style type="text/css">.findusContainer {
+	font-family: Calibri;
+	font-size: medium;
+	height:450px;
+	width: inherit;
+	margin-left:210px;
+	margin-right:150px;
+	color: #f1dca7;
+}
+
+.findusContainer p {
+	padding-top:30px;
+	padding-left:150px;
+}
+
+.findusContainer img {
+	float:right;
+	position: absolute;
+    top: 250px;
+    right: 250px;
+}
+.googlemap {
+	padding-left:150px;
+	height: 130px;
+	width: 400px;
+}
+
+</style>
+
+</head>
+
+<body>
+
+<div class="findusContainer">
+
+<p style="width: 400px" ><!--Find us paragraph-->
+<font face="'Brush Script MT'" size="6">Find Us</font><!--heading-->
+<br>
+You can find us in the cultural square of Covent Garden, the home of style and performance. 
+<br><br> 
+Our restaurant is situated amongst the designer boutiques and theatres in the area; enjoy our wide 
+range of food and drink and relax after a day of shopping or before a production.
+<br><br>
+We are within walking distance of 2 of the major tube stations, Leicester Square and Covent Garden 
+itself, as well as multiple bus stations. They all offer efficient services until late, so take your time!
+</p><!--end events paragraph-->
+
+<iframe class ="googlemap" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d19864.64710192255!2d-0.12327!3d51.51173200000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487604cb878e81b3%3A0x91f10fa364452046!2sCovent+Garden%2C+London+WC2E%2C+UK!5e0!3m2!1sen!2suk!4v1457958838489"
+ frameborder="0" style="border:0" ></iframe>
+
+<img alt="" height="350" src="images/find_us.jpg" width="600"></div>
+
+
+</body>
